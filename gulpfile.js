2d046e5023d91b930177036a2171bb9b3e78f920
Ariana-W <ariana.waller@yahoo.com>
2018-05-19 15:56:40 -0400

    added hero image

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('holla', function() {
+console.log('YO');
+});
+
+gulp.task('sass', function() {
+return gulp.src('src/scss/style.scss')
+.pipe(sass())
+.pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('default', function() {
+gulp.watch(['src/scss/*', 'src/scss/**/*'], ['sass']);
+});
